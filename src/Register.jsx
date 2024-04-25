4ad012aec47afdcfedcabdece5cb14d5aeeb16c4
Bond C <101323288+BondChen1229@users.noreply.github.com>
2024-04-26 00:40:30 +1200

    work 3

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,55 +1,33 @@
-import React, {useState} from 'react';
-function Register(){
-    const [formData, setFormData] = useState(
-        {
-            username:"",
-            email:"",
-            password:"",
-        }
-    );
+import React, { useState } from 'react';
 
-    const handleChange = (e) =>{
-        setFormData({
-            ...formData,
-            [e.target.name]:e.target.value
-        });
+function Register() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // SEND REQUIRE TO BACKEND
+        console.log(`Username: ${username}, Password: ${password}, Email: ${email}`);
     };
-    const handleSubmit = (e) =>{
-        e.preventDefault();//Add the logic that can be sent to the server into here
-        console.log(FormData);
-    };
+
     return (
         <form onSubmit={handleSubmit}>
-          <h2>Register</h2>
-          <div>
-            <label>Username</label>
-            <input
-              type="text"
-              name="username"
-              value={formData.username}
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <label>Email</label>
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <label>Password</label>
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-            />
-          </div>
-          <button type="submit">Submit</button>
+            <label>
+                Username:
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
+            </label>
+            <label>
+                Password:
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+            </label>
+            <label>
+                Email:
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+            </label>
+            <input type="submit" value="register" />
         </form>
-      );
+    );
 }
+
 export default Register;
