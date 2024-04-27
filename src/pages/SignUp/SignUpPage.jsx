48c8cb4bbebeb360487ae67bcb5bf2192b645395
Bond C <101323288+BondChen1229@users.noreply.github.com>
2024-04-27 17:57:38 +1200

    27/4

--- src/pages/SignUp/SignUpPage.jsx
+++ src/pages/SignUp/SignUpPage.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-
 function RegisterPage() {
   const [formData, setFormData] = useState({
     username: '',
     email: '',
-    password: ''
+    password: '',
+    confirm_password: '',
   });
+  const [error, setError] = useState('');
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -14,7 +15,12 @@ function RegisterPage() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (formData.password !== formData.confirm_password){
+        setError("The passwords you entered twice do not match");
+        return;
+    }
     console.log(formData);
+    setError('');
     // 这里可以加上发送数据到后端的代码
   };
 
@@ -25,39 +31,28 @@ function RegisterPage() {
         <div>
           <label>
             UserName:
-            <input
-              type="text"
-              name="username"
-              value={formData.username}
-              onChange={handleChange}
-              required
-            />
+            <input type="text" name="username" value={formData.username} onChange={handleChange}required/>
           </label>
         </div>
         <div>
           <label>
             Email:
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
+            <input type="email" name="email" value={formData.email} onChange={handleChange} required/>
           </label>
         </div>
         <div>
           <label>
             Password:
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              required
-            />
+            <input type="password" name="password" value={formData.password} onChange={handleChange}required/>
           </label>
         </div>
+        <div>
+            <label>
+                Confirm your Password:
+                <input type = "password" name="confirm_password" value={formData.confirm_password} onChange={handleChange} required/>
+            </label>
+        </div>
+        {error && <p className='error'></p>}
         <div>
           <button type="submit">SUBMIT</button>
         </div>
