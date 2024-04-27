12e09db22529b691cd3efa02b616eed678523965
Bond C <101323288+BondChen1229@users.noreply.github.com>
2024-04-27 16:54:10 +1200

    27/4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./router"
+import App from "."
 ReactDOM.render(
     <AppRouter/>,
     document.getElementById('root')
